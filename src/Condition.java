3f0e6e2c76be433c45bfc36b41b32f0249b53fb5
remycrt <remy.cortina@gmail.com>
2016-01-02 16:03:04 +0100

    fonction interpret à revoir

--- src/Condition.java
+++ src/Condition.java
@@ -2,16 +2,29 @@ import java.util.Map;
 
 public class Condition implements Expression
 {
-
+	//ATTRIBUTS
+	private String If;
+	private String Then;
+	private String Else;
+	
 	public Condition(String If, String Then, String Else)
 	{
-		
+		this.If=If;
+		this.Then=Then;
+		this.Else=Else;
 	}
 	
 	
-	public int interpret(Map<String, Expression> variables) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int interpret(Map<String, Expression> variables) 
+	{
+		if(If)
+		{
+			return Then;
+		}
+		else
+		{
+			return Else;
+		}
 	}
 
 }
